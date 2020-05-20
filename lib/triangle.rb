f451dca2cf66840ed2c362a5dcc59b5e28d3e230
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-20 19:38:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,9 +26,7 @@ class Triangle
       @side_1 > 0 &&
       @side_2 > 0 &&
       @side_3 > 0 &&
-      sides > @side_1 ||
-      sides > @side_2 ||
-      sides > @side_3
+      sides > @side_1 || sides > @side_2 || sides > @side_3
 
       if
         @side_1 != @side_2 &&
