6c3f1707a76430264f62992d16a0b048b33a34d8
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-20 19:25:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,22 +6,22 @@ class Triangle
     @side_3 = side_3
   end
 
-  def legal_triangle?
-    sides = @side_1 + @side_2 + @side_3
-    if
-      @side_1 > 0 &&
-      @side_2 > 0 &&
-      @side_3 > 0 &&
-      # sides > @side_1 &&
-      # sides > @side_2 &&
-      # sides > @side_3 &&
-      true
-    end
-  end
+  # def legal_triangle?
+  #   sides = @side_1 + @side_2 + @side_3
+  #   if
+  #     @side_1 > 0 &&
+  #     @side_2 > 0 &&
+  #     @side_3 > 0 &&
+  #     # sides > @side_1 &&
+  #     # sides > @side_2 &&
+  #     # sides > @side_3 &&
+  #     true
+  #   end
+  # end
 
 
   def kind
-    if legal_triangle?
+    if self.legal_triangle?
       if
         @side_1 != @side_2 &&
         @side_1 != @side_3 &&
