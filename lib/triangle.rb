113f7c42f3dce58195951dc3ad26bd8dc78c6b9b
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-20 19:44:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,9 +12,9 @@ class Triangle
       @side_1 > 0 &&
       @side_2 > 0 &&
       @side_3 > 0 &&
-      sides > @side_1 &&
-      sides > @side_2 &&
-      sides > @side_3 &&
+      sides > @side_1 ||
+      sides > @side_2 ||
+      sides > @side_3
       true
     end
   end
@@ -23,9 +23,7 @@ class Triangle
   def kind
     sides = @side_1 + @side_2 + @side_3
     if
-      @side_1 > 0 &&
-      @side_2 > 0 &&
-      @side_3 > 0 &&
+      @side_1 > 0 && @side_2 > 0 && @side_3 > 0 &&
       sides > @side_1 || sides > @side_2 || sides > @side_3
 
       if
