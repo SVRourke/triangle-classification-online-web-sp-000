aa5ac58213d8c04ac9e631e0bee41fcd476d9d8d
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-20 19:32:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,12 +23,8 @@ class Triangle
   def kind
     sides = @side_1 + @side_2 + @side_3
     if
-      @side_1 > 0 &&
-      @side_2 > 0 &&
-      @side_3 > 0 &&
-      sides > @side_1 or
-      sides > @side_2 or
-      sides > @side_3
+      @side_1 > 0 && @side_2 > 0 && @side_3 > 0 &&
+      sides > @side_1 or sides > @side_2 or sides > @side_3
       if
         @side_1 != @side_2 &&
         @side_1 != @side_3 &&
